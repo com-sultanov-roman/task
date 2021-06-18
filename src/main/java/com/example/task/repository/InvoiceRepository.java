8f58a37b090d40b96277983e036bf5ac35581749
com-sultanov-roman <com.sultanov.roman@gmail.com>
2021-06-18 14:41:32 +0500

    /overpaid_invoices fixed

--- src/main/java/com/example/task/repository/InvoiceRepository.java
+++ src/main/java/com/example/task/repository/InvoiceRepository.java
@@ -18,7 +18,14 @@ public interface InvoiceRepository extends CrudRepository<Invoice, Integer>{
 
     Invoice getInvoiceById(int id);
 
-    @Query(value = "SELECT * FROM (SELECT i.id as invoice_id, SUM(p.amount) - i.amount as amount_to_be_reimbursed  FROM invoice i JOIN payment p ON i.id = p.inv_id GROUP BY p.amount, i.id) X WHERE amount_to_be_reimbursed > 0", nativeQuery = true)
+//    @Query(value = "SELECT * FROM (SELECT i.id as invoice_id, SUM(p.amount) - i.amount as amount_to_be_reimbursed  FROM invoice i JOIN payment p ON i.id = p.inv_id GROUP BY p.amount, i.id) X WHERE amount_to_be_reimbursed > 0", nativeQuery = true)
+//    List<OverpaidInvoicesDTO> getOverpaidInvoices();
+
+    @Query(value = "SELECT * FROM(\n" +
+            "SELECT invoice_id, SUM(payment_amount) - invoice_amount  as amount_to_be_reimbursed\n" +
+            "FROM(SELECT i.id as invoice_id, i.amount as invoice_amount, p.id as payment_id, p.amount as payment_amount \n" +
+            "FROM invoice i, payment p WHERE i.id = p.inv_id ORDER BY i.id) X GROUP BY invoice_id, invoice_amount ORDER BY invoice_id\n" +
+            ") Y WHERE amount_to_be_reimbursed > 0;", nativeQuery = true)
     List<OverpaidInvoicesDTO> getOverpaidInvoices();
 
 
